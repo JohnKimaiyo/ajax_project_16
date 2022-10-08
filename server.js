bdeea530c8d33c4e8577bba1c4492a276fc77091
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-08 12:27:32 +0300

    settign up the server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./router');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+// consfigure bodyparser
+const jsonParser = bodyParser.json();
+const urlEncodedParser = bodyParser.urlencoded({ extnded: false });
+app.use(jsonParser);
+app.use(urlEncodedParser);
+
+// configure cors
+app.use(cors());
+
+// configure the router
+app.use('/api', router)
+
+// get
+app.get('/', (request, response) => {
+    response.send(`<h2>welcome to express server of employee portal </h2>`);
+});
+
+app.listen(port, hostname, () => {
+    console.log(`express server is started at http://${hostname} : ${port}`)
+});
