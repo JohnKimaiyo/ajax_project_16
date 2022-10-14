500c746cb76282c9f55a2edc61a3d6119ac61e2e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-14 12:04:57 +0300

    post http request

---
+++ brainhttp.js
@@ -0,0 +1,34 @@
+export class brainhttp {
+    constructor() {
+        this.http = new XMLHttpRequest();
+    }
+
+    // get request
+    get = (url, callback) => {
+        this.http.open('GET', url, true);
+        this.http.sebd();
+        this.http.onload = () => {
+            if (this.http.status === 200) {
+                let data = this.http.responseText;
+                let employees = JSON.parse(data)
+                callback(users);
+            }
+            else {
+                callback(`error:${this.htttp.status}`)
+            }
+        }
+    }
+}
+
+// post request
+post = (url, employees, callback) => {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('content-Type', 'application/json');
+    this.http.send(JSON.stringifly(employees));
+    this.http.onload = () => {
+        let dat = this.http.responseText;
+        let employees = JSON.parse(data)
+        callback(employees);
+    }
+}
+
